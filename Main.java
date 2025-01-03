bb5dd9fd5e55a018044d26606416740418c06117
Rajasekar <Rajasekaran.s@kgkite.ac.in>
2025-01-03 16:15:26 +0530

    greet function added

--- Main.java
+++ Main.java
@@ -1,4 +1,7 @@
 class Main{
+  public static void greet(String name){
+    System.out.println("Hello "+name+"!");
+  }
   public static void main(String[] args){
     System.out.println("Hello World");
   }
