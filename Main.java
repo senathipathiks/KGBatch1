44b893221f85b83cc52b831806e8b05daaf3c3c6
sowmya <sowmiyah13494@gmail.com>
2025-01-03 16:17:25 +0530

    print function created

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
-class Main{
-  public static void main(String[] args){
+class Main {
+  // Function to print a message
+  public static void printMessage() {
     System.out.println("Hello World");
   }
+
+  public static void main(String[] args) {
+    // Call the function
+    printMessage();
+  }
 }
+
